999864b2d417a635d4a4766ff3421fd081774b9d
ArtemZolotukh1n <90614769+ArtemZolotukh1n@users.noreply.github.com>
2023-03-04 16:25:15 +0300

    fix,refactor: Now after pressing add button recomposition is triggered immediately.
    Now adding, loading and saving pictures is done not in main thread.

--- app/src/main/java/com/example/mygalary/MainActivity.kt
+++ app/src/main/java/com/example/mygalary/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.example.mygalary
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mygalary.model.Bebra
-import com.example.mygalary.model.GalaryViewModel
 import com.example.mygalary.ui.theme.MyGalaryTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/mygalary/model/GalaryViewModel.kt
+++ app/src/main/java/com/example/mygalary/model/GalaryViewModel.kt
@@ -1,21 +1,28 @@
 package com.example.mygalary.model
 
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mygalary.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class GalaryViewModel(
-    private val savedStateHandle: SavedStateHandle
-) : ViewModel() {
-        val loadedPics = savedStateHandle.getStateFlow(
-        "pics", initialValue = mutableListOf<Bebra>(
-            Bebra(R.string.photo_1, R.string.desc_1, R.drawable.bebra_pic_1, R.string.add_desc_1),
-            Bebra(R.string.photo_2, R.string.desc_2, R.drawable.bebra_pic_2, R.string.add_desc_2),
-            Bebra(R.string.photo_3, R.string.desc_3, R.drawable.bebra_pic_3, R.string.add_desc_3)
-        )
-    )
+class GalaryViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val _loadedPics = MutableStateFlow(emptyList<Bebra>())
+    val loadedPics: StateFlow<List<Bebra>> = _loadedPics
 
+    init {
+        viewModelScope.launch {
+            _loadedPics.value = loadPhotosFromPref()
+        }
+    }
 
     fun addPicture() {
         val additionalBebraPhoto = Bebra(
@@ -24,6 +31,52 @@ class GalaryViewModel(
             R.drawable.placeholder_cat,
             R.string.add_desc_placeholder
         )
-        savedStateHandle.get<MutableList<Bebra>>("pics")!!.add(additionalBebraPhoto)
+        viewModelScope.launch {
+            val updatedList = _loadedPics.value.toMutableList()
+            updatedList.add(additionalBebraPhoto)
+            _loadedPics.value = updatedList
+            savePhotosToPrefs(updatedList)
+        }
+    }
+
+    private suspend fun savePhotosToPrefs(photos: List<Bebra>) {
+        withContext(Dispatchers.IO) {
+            val prefs =
+                getApplication<Application>().getSharedPreferences("photos", Context.MODE_PRIVATE)
+            val json = Gson().toJson(photos)
+            prefs.edit().putString("photos", json).apply()
+        }
+    }
+
+    private suspend fun loadPhotosFromPref(): MutableList<Bebra> {
+        return withContext(Dispatchers.IO) {
+            val prefs =
+                getApplication<Application>().getSharedPreferences("photos", Context.MODE_PRIVATE)
+            val json = prefs.getString("photos", null)
+            if (json != null) {
+                Gson().fromJson(json, object : TypeToken<MutableList<Bebra>>() {}.type)
+            } else {
+                mutableListOf(
+                    Bebra(
+                        R.string.photo_1,
+                        R.string.desc_1,
+                        R.drawable.bebra_pic_1,
+                        R.string.add_desc_1
+                    ),
+                    Bebra(
+                        R.string.photo_2,
+                        R.string.desc_2,
+                        R.drawable.bebra_pic_2,
+                        R.string.add_desc_2
+                    ),
+                    Bebra(
+                        R.string.photo_3,
+                        R.string.desc_3,
+                        R.drawable.bebra_pic_3,
+                        R.string.add_desc_3
+                    )
+                )
+            }
+        }
     }
 }
